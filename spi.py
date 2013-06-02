3af590e3b04c3c42a0eb5af17c5159dad0fc5c2d
Luciano Ramalho <luciano@ramalho.org>
2013-06-02 06:40:44 +0000

    exemplo se comunicacao SPI com MCP3008 OK

--- spi.py
+++ spi.py
@@ -6,19 +6,19 @@ import os
 import atexit
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
-
 # assegurar que a função cleanup será chamada na saída do script
 atexit.register(GPIO.cleanup)
 
-# change these as desired - they're the pins connected from the
-# SPI port on the ADC to the Cobbler
+# usar numeração lógica dos pinos
+GPIO.setmode(GPIO.BCM)
+
+# definir pinos que serão usados para implementar SPI
 SPICLK = 18
 SPIMISO = 23
 SPIMOSI = 24
 SPICS = 25
 
-# set up the SPI interface pins
+# configurar pinos
 GPIO.setup(SPICLK, GPIO.OUT)
 GPIO.setup(SPIMISO, GPIO.IN)
 GPIO.setup(SPIMOSI, GPIO.OUT)
@@ -26,38 +26,39 @@ GPIO.setup(SPICS, GPIO.OUT)
 
 # ler dados do MCP3008 usando protocolo SPI
 def ler_canal(canal):
-        assert 0 <= canal <= 7, 'canal deve ser de 0 a 7'
-        GPIO.output(SPICS, True)
-        GPIO.output(SPICLK, False)  # start clock low
-        GPIO.output(SPICS, False)   # bring CS low
-        print canal
-        cmd = canal
-        print '{0:08b}'.format(cmd)
-        cmd |= 0x18  # start bit + single-ended bit
-        print '{0:08b}'.format(cmd)
-        cmd <<= 3    # we only need to send 5 bits here
-        print '{0:08b}'.format(cmd)
-        for i in range(5):
-                GPIO.output(SPIMOSI, cmd & 0x80)
-                cmd <<= 1
-                GPIO.output(SPICLK, True)
-                GPIO.output(SPICLK, False)
+    assert 0 <= canal <= 7, 'canal deve ser de 0 a 7'
+    GPIO.output(SPICS, True)    
+    GPIO.output(SPICLK, False)
+    GPIO.output(SPICS, False)
+    cmd = canal
+    cmd |= 0x18  # bit inicial + bit configuração "single-ended"
+    cmd <<= 3    # descartar 3 bits; usaremos apenas 5
+    for i in range(5):
+        GPIO.output(SPIMOSI, cmd & 0x80)
+        cmd <<= 1
+        GPIO.output(SPICLK, True)
+        GPIO.output(SPICLK, False)
 
-        res = 0
-        # read in one empty bit, one null bit and 10 ADC bits
-        for i in range(12):
-                GPIO.output(SPICLK, True)
-                GPIO.output(SPICLK, False)
-                res <<= 1
-                if (GPIO.input(SPIMISO)):
-                        res |= 0x1
+    res = 0
+    # ler bit nulo e 10 bits do valor ADC
+    for i in range(11):
+        GPIO.output(SPICLK, True)
+        GPIO.output(SPICLK, False)
+        res <<= 1
+        if (GPIO.input(SPIMISO)):
+            res |= 0x1
 
-        GPIO.output(SPICS, True)
-        print '{0:012b}'.format(res)
-        res >>= 1       # first bit is 'null' so drop it
-        return res
+    GPIO.output(SPICS, True)
+    return res
 
-import pdb; pdb.set_trace()
-print 'lido:', ler_canal(3)
-
-
+#import pdb; pdb.set_trace()
+contador = 0
+display = '{0:6d}  {1:010b}  {1:4}  {2:3.2f} V {3}'
+while True:
+    res = ler_canal(3)
+    volts = float(res)/1023 * 3.3
+    marcas = int(round(float(res)/1023*40))*'='
+    print display.format(contador, res, volts, marcas)
+    time.sleep(.2)
+    contador += 1
+    
